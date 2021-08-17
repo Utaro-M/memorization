fef9ab8e7a343c244cbac6c8cd5d943e806e8dfd
matsuura <matsuura@jsk.imi.i.u-tokyo.ac.jp>
2021-08-17 05:43:21 +0000

    get target pos and radious

--- script/get_grab_pose.py
+++ script/get_grab_pose.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-import message_filters
-import tf2_ros
-import tf2_geometry_msgs
-from cv_bridge import CvBridge
-import cv2
-from sensor_msgs.msg import Image
-from sensor_msgs.msg import CameraInfo
-from sensor_msgs.msg import Joy
-from geometry_msgs.msg import PointStamped
-from geometry_msgs.msg import PolygonStamped
-from geometry_msgs.msg import Polygon #test
-from geometry_msgs.msg import PoseStamped
-
 from jsk_recognition_msgs.msg import BoundingBoxArray
 
 class GetGrabPose(object):
     def __init__(self):
         rospy.Subscriber('~bbox', BoundingBoxArray, self.get_params)
-        # rospy.Subscriber('/hand_poses', BoundingBoxArray, self.get_poses)
         self.center = []
         self.r = []
         self.tgt_num = 1
@@ -27,12 +13,13 @@ class GetGrabPose(object):
         
     def get_params(self,msg):
         boxes = msg.boxes
-        boxes_list =np.array([[]])
-        dimension_list = np.array([[]])
+        boxes_list =np.array([[0,0,0]])
+        dimension_list = np.array([[0,0,0]])
         for box in boxes:
-            print("np.array([[box.pose.position.x, box.pose.position.y, box.pose.position.z]] = {}".format(np.array([[box.pose.position.x, box.pose.position.y, box.pose.position.z]])))
-            boxes_list = np.append(boxes_list,np.array([[box.pose.position.x, box.pose.position.y, box.pose.position.z]]),axis=1)
-            dimension_list = np.append(dimension_list,np.array([[box.dimensions.x, box.dimensions.y, box.dimensions.z]]),axis=1)
+            boxes_list = np.append(boxes_list,np.array([[box.pose.position.x, box.pose.position.y, box.pose.position.z]]),axis=0)
+            dimension_list = np.append(dimension_list,np.array([[box.dimensions.x, box.dimensions.y, box.dimensions.z]]),axis=0)
+        boxes_list = boxes_list[1:]
+        dimension_list = dimension_list[1:]
         sorted_index = np.argsort(map(lambda x :abs(x[0]*x[1]*x[2]),dimension_list)[::-1])[:self.tgt_num] #extract self.tgt_num
         boxes_list = boxes_list[sorted_index]
         dimension_list = dimension_list[sorted_index]
